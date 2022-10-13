331d6d7604b2131cbccdf353b442320c884947ef
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-13 20:07:10 +0000

    Adding tarfile member sanitization to extractall()

--- Generative Models/HW3_extra_files/download_lfw.py
+++ Generative Models/HW3_extra_files/download_lfw.py
@@ -35,7 +35,29 @@ def download_lfw_dataset(url, filename):
 					
 	#extruct .tar file
 	with tarfile.open(filename, 'r') as f:
-		f.extractall(data_directory)
+
+import os
+
+def is_within_directory(directory, target):
+	
+	abs_directory = os.path.abspath(directory)
+	abs_target = os.path.abspath(target)
+
+	prefix = os.path.commonprefix([abs_directory, abs_target])
+	
+	return prefix == abs_directory
+
+def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+
+	for member in tar.getmembers():
+		member_path = os.path.join(path, member.name)
+		if not is_within_directory(path, member_path):
+			raise Exception("Attempted Path Traversal in Tar File")
+
+	tar.extractall(path, members, numeric_owner=numeric_owner) 
+	
+
+safe_extract(f, data_directory)
 
 
 if __name__ == "__main__":
